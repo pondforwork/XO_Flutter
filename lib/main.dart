7ab7b8f39f61b591d34724449b6f9c8ffe9658dc
pondforwork <pharadonsiriforwork@gmail.com>
2023-12-08 13:24:48 +0700

    Select Player Page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:xo_flutter/screen/selectplayer.dart';
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  selectplayer(),
+    );
+  }
+}
---
+++ lib/screen/selectplayer.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class selectplayer extends StatefulWidget {
+  const selectplayer({super.key});
+
+  @override
+  State<selectplayer> createState() => _selectplayerState();
+}
+
+class _selectplayerState extends State<selectplayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+        body: Column(
+      children: [
+        const SizedBox(
+          height: 200,
+        ),
+        const Column(
+          children: [
+            Center(
+              child: Text(
+                "Select First Player",
+                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 150,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Card(
+                  color: Colors.blue, // Set the background color here
+                  clipBehavior: Clip.hardEdge,
+                  child: InkWell(
+                    splashColor: Colors.green,
+                    onTap: () {
+                      print("Test");
+                    },
+                    child: const SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Center(
+                        child: SizedBox(
+                          width: 150,
+                          height: 150,
+                          child: Center(
+                            child: Text(
+                              'X',
+                              style:
+                                  TextStyle(fontSize: 80, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )),
+            ),
+            Card(
+                color: Colors.red, // Set the background color here
+
+                clipBehavior: Clip.hardEdge,
+                child: InkWell(
+                  splashColor: Colors.green,
+                  onTap: () {
+                    print("Test");
+                  },
+                  child: const SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: Center(
+                      child: Text(
+                        'O',
+                        style: TextStyle(fontSize: 80, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        )
+      ],
+    ));
+  }
+}
